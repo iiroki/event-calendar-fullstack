658f2d659c5a3ccbed880016b3de40af1c215b46
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-09-29 10:38:12 +0300

    Calendar keeps track of the last viewed month (navigating back doesn't reset back to today).

---
+++ client/src/reducers/selectedDateReducer.js
@@ -0,0 +1,18 @@
+const { act } = require("react-dom/test-utils");
+
+const selectedDateReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'SET_SELECTED_DATE':
+      return action.data
+
+    default:
+      return state
+  }
+}
+
+export const setSelectedDate = date => ({
+  type: 'SET_SELECTED_DATE',
+  data: date
+})
+
+export default selectedDateReducer
