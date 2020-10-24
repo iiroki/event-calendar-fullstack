27dfc60b3be4f7679c8d129ccfd3b2559114dd10
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-10-24 09:22:42 +0300

    ics timezone hotfix v2

--- client/src/utils/icsConverter.js
+++ client/src/utils/icsConverter.js
@@ -5,12 +5,14 @@ export const eventToIcs = (eventObject, organizer) => {
   const utcOffset = moment().utcOffset() / 60
   const s = moment(eventObject.start) // start date
   const e = moment(eventObject.end) // end date
+  s.hour(s.hour() - utcOffset)
+  e.hour(e.hour() - utcOffset)
 
   const eventForIcs = {
     title: eventObject.title,
     location: eventObject.location,
-    start: [s.year(), s.month() + 1, s.date(), s.hour() - utcOffset, s.minute()],
-    end: [e.year(), e.month() + 1, e.date(), e.hour() - utcOffset, e.minute()],
+    start: [s.year(), s.month() + 1, s.date(), s.hour(), s.minute()],
+    end: [e.year(), e.month() + 1, e.date(), e.hour(), e.minute()],
     description: eventObject.description,
     productId: 'teekkarikalenteri/ics'
   }
@@ -18,6 +20,7 @@ export const eventToIcs = (eventObject, organizer) => {
   const { error, value } = ics.createEvent(eventForIcs)
 
   if (error) {
+    console.log(error)
     return null
   }
   
