4faec24bfa92956a638c5f92d47388e476dc6977
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-11-02 14:28:01 +0200

    Treekkarit theme

--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -214,7 +214,10 @@ const UserInformationForm = ({ profile, handleHide = null }) => {
       </div>
 
       <div>
-        <button type='submit' className='btn btn-danger'>
+        <button
+          type='submit'
+          className='btn btn-treekkari'
+        >
           Vahvista
         </button>
       </div>
@@ -345,7 +348,10 @@ const NewPasswordForm = ({ id, handleHide = null }) => {
       </div>
 
       <div>
-        <button type='submit' className='btn btn-danger'>
+        <button
+          type='submit'
+          className='btn btn-treekkari'
+        >
           Vahvista
         </button>
       </div>
@@ -362,7 +368,7 @@ const ProfileForm = ({ profile, handleHide }) => {
     <div>
       <div className='btn-group btn-group-toggle button-group' data-toggle='buttons'>
         <label // eslint-disable-line
-          className='btn btn-danger active'
+          className='btn btn-treekkari active'
           onClick={() => setEditPassword(false)}
         >
           <input type='radio' name='options' id='option1' autoComplete='off' />
@@ -370,7 +376,7 @@ const ProfileForm = ({ profile, handleHide }) => {
         </label>
 
         <label // eslint-disable-line
-          className='btn btn-danger'
+          className='btn btn-treekkari'
           onClick={() => setEditPassword(true)}
         >
           <input type='radio' name='options' id='option2' autoComplete='off' />
