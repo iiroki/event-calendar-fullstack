cb413e3658ef5683e0769b4dfc920c0503326560
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-11-16 18:14:41 +0200

    Started writing AboutPage.

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
 const AboutPage = () => (
-  <div>
-    Yleistä kalenterista (TBD)
+  <div className='about-box'>
+    <h4>
+      <b>
+        Yleistä kalenterista
+      </b>
+    </h4>
+    <p>
+      Teekkarikalenteri on PerinneSeuran toimittama ja Tampereen
+      Teekkarien julkaisema tamperelainen teekkarikalenteri.
+    </p>
+    
+    <h4>
+      <b>
+        Kalenterin tilaaminen toiseen kalenteriin
+      </b>
+    </h4>
+    <p>
+      Teekkarikalenterin voi tilata toiseen kalenteriin alla olevalla
+      URL-osoitteella:
+      <br />
+      <span className='subscribe-link'>
+        <i>
+          https://teekkarikalenteri.fi/api/subscribe/teekkarikalenteri.ics 
+        </i>
+      </span>
+    </p>
   </div>
 )
 
