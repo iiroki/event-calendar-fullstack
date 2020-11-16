1b24c0283b5a1347d77578a4c88edaeeb5965d0b
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-11-16 17:21:40 +0200

    AdBox almost ready.

--- client/src/components/AdBox.js
+++ client/src/components/AdBox.js
@@ -1,34 +1,54 @@
 import React from 'react'
 import Slider from 'react-slick'
 
+const AdImage = ({ path, link, alt}) => (
+  <div className='ad-container'>
+    <a
+      href={`//${link}`}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <img className='ad' src={path} alt={alt} />
+    </a>
+  </div>
+)
+
 const AdBox = () => (
   <div className='adbox'>
     <Slider
-      infinite
       autoplay
-      autoplaySpeed={3000}
-      speed={1000}
-      pauseOnHover
+      autoplaySpeed={5000}
+      speed={1800}
       slidesToShow={3}
+      pauseOnHover
       dots
       arrows={false}
       centerMode
+      centerPadding='10px'
     >
-      <div>
-        1
-      </div>
-      <div>
-        2
-      </div>
-      <div>
-        3
-      </div>
-      <div>
-        4
-      </div>
-      <div>
-        5
-      </div>
+      <AdImage
+        path='/ads/ad_tek.png'
+        link='www.tek.fi'
+        alt='Tekniikan Akateemiset'
+      />
+
+      <AdImage
+        path='/ads/ad_kattosauna.png'
+        link='kattosauna.fi'
+        alt='Hämpin Kattosauna'
+      />
+
+      <AdImage
+        path='/ads/ad_tk.png'
+        link='tekniikankutsu.com'
+        alt='Tekniikan Kutsu'
+      />
+
+      <AdImage
+        path='/ads/ad_np.png'
+        link='nokianpanimo.fi'
+        alt='Nokian Panimo'
+      />
     </Slider>
   </div>
 )
