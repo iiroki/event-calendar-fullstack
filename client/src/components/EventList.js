af74ee0f0d7369e27e55444cac76bc5b0d11ff33
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-09 16:34:34 +0200

    Added search form and user dropdown, functionality not implemented yet!

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -14,10 +14,12 @@ const EventListItem = ({ e }) => {
     <div
       className='event-list-item'
       role='button'
+      onClick={handleEventSelect}
+      tabIndex={0}
+      onKeyPress={handleEventSelect}
       style={{
         backgroundColor: `#${e.bgColor}`
       }}
-      onClick={handleEventSelect}
     >
       <div
         className='event-list-item-box'
@@ -33,14 +35,46 @@ const EventListItem = ({ e }) => {
     </div>
   )
 }
+
+const UserDropdownItem = ({ u }) => (
+  <span className='dropdown-item user-dropdown-item'>
+    {u.name}
+  </span>
+)
+
 const EventList = () => {
   const [search, setSearch] = useState('') // Search by event name
-  const [userFilter, setUserFilter] = useState('') // Filter events by user
+  const [userFilter, setUserFilter] = useState('Etsi käyttäjää') // Filter events by user
 
+  const users = useSelector(state => state.users)
   const events = useSelector(state => state.events)
 
   return (
     <div className='event-list'>
+      <input
+        type='text'
+        className='form-control'
+        id='searchInput'
+        value={search}
+        onChange={({ target }) => setSearch(target.value)}
+        placeholder='Etsi tapahtuman nimellä'
+      />
+
+      <div className='user-dropdown'>
+        <button className='btn dropdown-toggle btn-treekkari' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+          {userFilter}
+        </button>
+        <div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+          <span className='dropdown-item user-dropdown-item'>
+            Ei valittu
+          </span>
+          <hr style={{ margin: '2px 10px' }} />
+          {
+            users.map(u => <UserDropdownItem key={u.id} u={u} />)
+          }
+        </div>
+      </div>
+
       {
         events.map(e => <EventListItem key={e.id} e={e} />)
       }
