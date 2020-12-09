540f86014109b69c0d8390025c1d54cd7f98d0a4
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-09 10:36:34 +0200

    Added event list button to navbar. created event list template.

---
+++ client/src/components/EventList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const EventList = () => {
+  const [x, setX] = useState('')
+
+  return (
+    <div>
+      Lista
+    </div>
+  )
+}
+
+export default EventList
