00a940c33bab0ea430de5cc1d48123add2a334d3
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2021-01-13 18:08:14 +0200

    Implemented about page.

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -8,7 +8,17 @@ const AboutPage = () => (
       </b>
     </h4>
     <p>
-      TBD
+      Kalenterin frontend on toteutettu Reactin ja Reduxin avulla ja
+      backendin päätyökaluina puolestaan toimi Node.js ja Express.
+      Kalenterin tietovarastona toimii MySQL-tietokanta.
+    </p>
+    <p>
+      Kalenterin avulla sisäänkirjautuneet käyttäjät kykenevät luomaan,
+      muokkaamaan ja poistamaan tapahtumia. Käyttäjät voivat myös muokata
+      profiiliaan, eli tapahtumiensa väriä sekä profiilin linkkiä.
+      Tapahtumia on mahdollisuus tarkastella niin kalenteri- kuin listanäkymässä,
+      josta löytyy lisäominaisuutena hakutoiminnallisuus. Yksittäistä tapahtumaa
+      klikkaamalla pääsee kyseisen tapahtuman omalle tapahtumasivulle.
     </p>
   </div>
 )
